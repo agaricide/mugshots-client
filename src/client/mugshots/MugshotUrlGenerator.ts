79d3a8cc505c500117ad42de0f28a291a83a2f08
agaricide <agaricide@gmail.com>
2019-03-11 22:22:50 -0500

    Implemented async iterator to traverse virtualized list of Mugshot urls

---
+++ src/client/mugshots/MugshotUrlGenerator.ts
@@ -0,0 +1,37 @@
+import { Browser, Page } from 'puppeteer';
+import { County } from '../types/County';
+
+const scrapeMugshotUrls = (page: Page) => page.evaluate(() => {
+  const els = document.querySelectorAll('a.image-preview');
+  return Array.from(els)
+    .filter(el => !el.querySelector('.no-image'))
+    .map(el => window.origin + el.getAttribute('href'));
+});
+
+const scrapeNextCountyPage = (page: Page) => page.evaluate(() => {
+  const nextHref = document
+    .querySelector('a[class*=next]')
+    .getAttribute('href');
+  return window.location.href + nextHref;
+});
+
+const is404 = (page: Page) => page.evaluate(() => {
+  return window.location.href.split('/').pop() === 'None' ? true : false;
+});
+
+const MugshotUrlGenerator = async (browser: Browser, county: County) => {
+  const page = await browser.newPage();
+  await page.goto(county.url);
+  return {
+    async *[Symbol.iterator]() {
+      while (!await is404(page)) {
+        const next = await scrapeNextCountyPage(page);
+        const urls = await scrapeMugshotUrls(page);
+        await page.goto(next);
+        yield urls;
+      }
+    }
+  };
+};
+
+export { MugshotUrlGenerator };
