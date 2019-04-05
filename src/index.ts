fb6be388c1184e81194a5450b87eed996e9ba8a4
agaricide <agaricide@gmail.com>
2019-04-04 23:58:39 -0500

    Adding new iterator to index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from './client/counties/CountyIterator';
+export * from './client/mugshots/MugshotUrlIterator';
 export * from './client/mugshots/scrapeMugshot';
-export * from './client/mugshots/scrapeMugshots';
 export * from './client/mugshots/scrapeMugshots';
 export * from './client/types/State';
