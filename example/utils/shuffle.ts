0b899a1c473e4eedbf89443e683f3da159a60df4
agaricide <agaricide@gmail.com>
2019-04-07 03:59:27 -0500

    Removing unused shuffle function

--- example/utils/shuffle.ts
+++
@@ -1,8 +0,0 @@
-// Durstenfeld shuffle, off of StackOverflow
-export function shuffle(array: any[]): any[] {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-}
