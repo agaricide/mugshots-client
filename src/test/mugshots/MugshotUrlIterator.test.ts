710451196867ff145c32b3cd8c1b8b9211caf14c
agaricide <agaricide@gmail.com>
2019-04-07 12:48:00 -0500

    Fixed tabs in test

--- src/test/mugshots/MugshotUrlIterator.test.ts
+++ src/test/mugshots/MugshotUrlIterator.test.ts
@@ -17,35 +17,35 @@ afterAll(async (done) => {
 });
 
 const county: County = {
-    name: 'Autauga County',
-    state: 'Alabama',
-    url: 'https://mugshots.com/US-Counties/Alabama/Autauga-County-AL/'
-  };
-  
-  describe('MugshotUrlIterator', () => {
-    it('is an async iterator that iterates through mugshot strings', async (done) => {
-      const page = await browser.newPage();
-      const mugshotUrls = await MugshotUrlIterator(page, county);
-      for await (const url of mugshotUrls) {
-        expect(url).toBeTruthy();
-        expect(typeof url).toBe('string');
-        break;
-      }
-      await page.close();
-      done();
-    });
+  name: 'Autauga County',
+  state: 'Alabama',
+  url: 'https://mugshots.com/US-Counties/Alabama/Autauga-County-AL/'
+};
+
+describe('MugshotUrlIterator', () => {
+  it('is an async iterator that iterates through mugshot strings', async (done) => {
+    const page = await browser.newPage();
+    const mugshotUrls = await MugshotUrlIterator(page, county);
+    for await (const url of mugshotUrls) {
+      expect(url).toBeTruthy();
+      expect(typeof url).toBe('string');
+      break;
+    }
+    await page.close();
+    done();
   });
-  
-  describe('MugshotUrlChunkIterator', () => {
-    it('is an async iterator that iterates through mugshot strings', async (done) => {
-      const page = await browser.newPage();
-      const mugshotUrls = await MugshotUrlChunkIterator(page, county);
-      for await (const chunk of mugshotUrls) {
-        expect(chunk.length).toBeTruthy();
-        expect(typeof chunk[0]).toBe('string');
-        break;
-      }
-      await page.close();
-      done();
-    });
+});
+
+describe('MugshotUrlChunkIterator', () => {
+  it('is an async iterator that iterates through mugshot strings', async (done) => {
+    const page = await browser.newPage();
+    const mugshotUrls = await MugshotUrlChunkIterator(page, county);
+    for await (const chunk of mugshotUrls) {
+      expect(chunk.length).toBeTruthy();
+      expect(typeof chunk[0]).toBe('string');
+      break;
+    }
+    await page.close();
+    done();
   });
+});
