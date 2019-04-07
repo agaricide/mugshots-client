519eb33c45b51c2489f08c07b0ea65d56a20fcde
agaricide <agaricide@gmail.com>
2019-04-06 20:20:12 -0500

    Experimenting with example

--- example/index.ts
+++ example/index.ts
@@ -4,13 +4,16 @@ import * as mongoose from 'mongoose';
 import * as puppeteer from 'puppeteer';
 import { MugshotModel } from './models/Mugshot';
 import { MugshotUrlChunkIterator, CountyIterator, scrapeMugshots } from '../src/index';
+import { performance } from 'perf_hooks';
 
 // Set environment variables
 if (process.env.NODE_ENV !== 'production') {
   dotenv.config();
 }
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+
+const average = (array: number[]) => array.reduce((p,c,_,a) => p + c/a.length,0);
 
 (async () => {
   console.log('Starting...');
@@ -18,15 +21,22 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
   console.log('Browser launched.');
   const counties = await CountyIterator(browser);
 
+  let runtimes = [];
   for await (const county of counties) {
     console.log(county.name);
+    const startTime = performance.now();
     const mugshotUrls = await MugshotUrlChunkIterator(browser, county);
-    console.log('Created mugshot iterator.')
-    for await (const chunk of mugshotUrls) {
-      console.log(chunk);
-      const mugshots = await scrapeMugshots(browser, chunk, { count: 100 });
-      console.log(mugshots);
-      MugshotModel.insertMany(mugshots);
-    }
+    const endTime = performance.now();
+    const runtime = endTime - startTime;
+    runtimes.push(runtime);
+    console.log(`runtime: ${runtime}`);
+    console.log(`avg: ${average(runtimes)}`);
+    // console.log('Created mugshot iterator.')
+    // for await (const chunk of mugshotUrls) {
+    //   console.log(chunk);
+    //   const mugshots = await scrapeMugshots(browser, chunk, { count: 100 });
+    //   console.log(mugshots);
+    //   MugshotModel.insertMany(mugshots);
+    // }
   }
 })();
