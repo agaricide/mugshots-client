0eb30635eb1ed305f35d13d717bd475ded77ce78
agaricide <agaricide@gmail.com>
2019-04-12 00:10:40 -0500

    Cleaned up MugshotStream example

--- example/stream.ts
+++ example/stream.ts
@@ -1,31 +1,23 @@
 import 'reflect-metadata';
-import * as dotenv from 'dotenv';
 import * as mongoose from 'mongoose';
-import { MugshotModel } from './models/Mugshot';
 import { MugshotStream, Mugshot } from '../src/index';
-
-// Set environment variables
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config();
-}
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
-// Ingest mugshots using iterables
+// Console.log mugshot data using stream API
 (async () => {
   const mugshotStream = await MugshotStream();
   console.log('Stream created.');
 
   mugshotStream.on('error', (error) => {
-      console.log(error);
+    console.log(error);
   });
 
   mugshotStream.on('close', () => {
-      console.log('Stream closed.');
+    console.log('Stream closed.');
   });
 
   mugshotStream.on('data', (mugshots: Mugshot[]) => {
     console.log('data', mugshots);
   });
 })();
-
