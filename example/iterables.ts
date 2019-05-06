c07c1b627ad51c1d88fa9231acbb3e052e6c3770
agaricide <agaricide@gmail.com>
2019-05-06 02:09:20 -0500

    NODE_ENV will never be production so remove check

--- example/iterables.ts
+++ example/iterables.ts
@@ -5,10 +5,7 @@ import * as puppeteer from 'puppeteer';
 import { MugshotModel } from './models/Mugshot';
 import { CountyIterable, MugshotUrlChunkIterable, scrapeMugshots, PagePool } from '../dist';
 
-// Set environment variables
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config();
-}
+dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
