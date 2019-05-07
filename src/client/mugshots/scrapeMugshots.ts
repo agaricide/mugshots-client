ddd2d5c62b3003f93a4d1049f1dfbfd466c7df6c
agaricide <agaricide@gmail.com>
2019-05-06 21:05:38 -0500

    Appended "max" to chunkSize to convey upper bounds

--- src/client/mugshots/scrapeMugshots.ts
+++ src/client/mugshots/scrapeMugshots.ts
@@ -3,17 +3,17 @@ import { scrapeMugshot } from './scrapeMugshot';
 import { Pool } from 'generic-pool';
 
 export interface ScrapeOptions {
-  chunkSize?: number
+  maxChunkSize?: number
 }
 
 const defaults: ScrapeOptions = {
-  chunkSize: 100
+  maxChunkSize: 100
 };
 
 export async function scrapeMugshots(pagePool: Pool<Page>, urls: string[], opts: ScrapeOptions = {}) {
   const options = {  ...defaults, ...opts };
   const mugshots = urls
-    .slice(0, options.chunkSize)
+    .slice(0, options.maxChunkSize)
     .map(async (url) => {
       const page = await pagePool.acquire();
       const mugshot = await scrapeMugshot(page, url);
