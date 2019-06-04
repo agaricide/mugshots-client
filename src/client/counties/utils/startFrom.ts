1a5744ea1cb480c93154fb97384d0adf64c75874
agaricide <agaricide@gmail.com>
2019-06-03 23:35:47 -0500

    KISS startFrom

--- src/client/counties/utils/startFrom.ts
+++ src/client/counties/utils/startFrom.ts
@@ -1,23 +1,18 @@
 import { County } from '../../types/County';
 import { State } from '../../types/State';
 
-interface args<T> {
-  prop: 'name' | 'state';
-  records: T[];
-  start?: County;
-}
-
-const startFrom = <T extends State | County>({ prop, records, start }: args<T>): T[] => {
+const startFromState = (records: State[], start?: County): State[] => {
   if (!start) return records;
-  const index = records.findIndex(record => record.name === start[prop]);
+  const index = records.findIndex(record => record.name === start.state);
   if (index === -1) return records;
   return records.slice(index);
 };
 
-const startFromState = (records: State[], start?: County) =>
-  startFrom({ prop: 'state', records, start });
-
-const startFromCounty = (records: County[], start?: County) =>
-  startFrom({ prop: 'name', records, start });
+const startFromCounty = (records: County[], start?: County): County[] => {
+  if (!start) return records;
+  const index = records.findIndex(record => record.name === start.name);
+  if (index === -1) return records;
+  return records.slice(index);
+};
 
 export { startFromState, startFromCounty };
